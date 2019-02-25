4392753df0c3249e54850aaef295fb35ff2a89fe
gbarrbrown <gbb7899@gmail.com>
2019-02-25 21:50:01 +1300

    react&redux return list of todos

---
+++ client/api/todos.js
@@ -0,0 +1,6 @@
+import request from 'superagent'
+
+export function getTodos(){
+    return request.get('/api/v1/todos')
+    .then((res) => res.body)
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
-const App = (props) => {
-    return (
-        <h1>TODOS!</h1>
-    )
+import { getTodos } from '../actions'
+
+class App extends Component {
+    
+    componentDidMount(){
+        this.props.dispatch(getTodos())
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>TODOS</h1>
+                <ul>
+                    {this.props.todos.map((item) => {
+                        return (
+                            <li>{item.task}</li>
+                        )
+                    })}
+                </ul>
+            </div>
+            )
+    }
 }
 
-export default App
+function mapStateToProps(state) {
+    return {
+        todos: state.todos
+    }
+}
+
+export default connect(mapStateToProps)(App)
